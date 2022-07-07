02ae210f84f1cbe98474725fb8517be032ab65c8
Mohanshiva2 <Mohanshiva2198@gmail.com>
2022-07-07 18:11:52 +0530

    v1

--- lib/main.dart
+++ lib/main.dart
@@ -57,15 +57,15 @@ class _MyHomePageState extends State<MyHomePage> {
               GestureDetector(
                 onTap: () {
                   setState(() {
-                    deviceStatus2 = !deviceStatus2;
-                    print(deviceStatus2);
+                    deviceStatus1 = !deviceStatus1;
+                    print(deviceStatus1);
                     http.put(
                       Uri.parse('http://54.85.222.247/gate/1/'),
                       headers: <String, String>{
                         'Content-Type': 'application/json; charset=UTF-8',
                       },
                       body: jsonEncode(<String, bool>{
-                        "Gate_Status": deviceStatus2,
+                        "Gate_Status": deviceStatus1,
                       }),
                     );
                   });
@@ -74,10 +74,10 @@ class _MyHomePageState extends State<MyHomePage> {
                   height: 100,
                   width: 200,
                   decoration: BoxDecoration(
-                      color: deviceStatus2 == true ? Colors.blue : Colors.white,
+                      color: deviceStatus1 == true ? Colors.blue : Colors.white,
                       borderRadius: BorderRadius.circular(30)),
                   child: Center(
-                    child: Text(deviceStatus2 == true ? 'Gate_Status On' : "Gate_Status Off"),
+                    child: Text(deviceStatus1 == true ? 'Gate_Status On' : "Gate_Status Off"),
                   ),
                 ),
               ),
@@ -88,12 +88,12 @@ class _MyHomePageState extends State<MyHomePage> {
                     deviceStatus2 = !deviceStatus2;
                     print(deviceStatus2);
                     http.put(
-                      Uri.parse('http://54.85.222.247/gate/1/'),
+                      Uri.parse('http://117.247.181.113:8000/23/'),
                       headers: <String, String>{
                         'Content-Type': 'application/json; charset=UTF-8',
                       },
                       body: jsonEncode(<String, bool>{
-                        "Gate_Status": deviceStatus2,
+                        "Device_Status": deviceStatus2,
                       }),
                     );
                   });
@@ -105,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       color: deviceStatus2 == true ? Colors.blue : Colors.white,
                       borderRadius: BorderRadius.circular(30)),
                   child: Center(
-                    child: Text(deviceStatus2 == true ? 'Gate_Status On' : "Gate_Status Off"),
+                    child: Text(deviceStatus2 == true ? 'Device_Status On' : "Device_Status Off"),
                   ),
                 ),
               ),
@@ -114,6 +114,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 onTap: () {
                   setState(() {
                     _databaseReference.child('gate').update({'gate_status': temvalue = !temvalue});
+                    print(temvalue);
                   });
                 },
                 child: Container(
@@ -123,7 +124,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       color: temvalue == true ? Colors.blue : Colors.white,
                       borderRadius: BorderRadius.circular(30)),
                   child: Center(
-                    child: Text(temvalue == true ? 'Gate_Status On' : "Gate_Status Off"),
+                    child: Text(temvalue == true ? 'FB Gate_Status On' : "FB Gate_Status Off"),
                   ),
                 ),
               ),
